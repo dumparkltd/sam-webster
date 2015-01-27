54d1627105ac2f3e6d3e42fd345c7b0e5e9d7059
Timo Franz <timo@dumpark.com>
2015-01-28 11:03:28 +1300

    re-organising css, adding timeline section

--- app/views/TimelineView.js
+++ app/views/TimelineView.js
@@ -1,19 +1,28 @@
 define([
   'jquery','underscore','backbone',  
-//  'views/ScrollView',  
+  'views/TimelineFramesView',//subviews
   'text!templates/timelineTemplate.html'
-], function($, _, Backbone, template){
+], function($, _, Backbone, TimelineFramesView, template){
 
   var TimelineView = Backbone.View.extend({
     initialize : function(){
       this.render();
+      this.hasFramesView = true;      
     },       
     events : {
     },    
     render: function(){         
       this.$el.html(_.template(template)({}));
-      return this;
-   },
+      this.framesView = new TimelineFramesView({
+        el:this.$('.frames-view'),
+        enable_scrolling:true,
+        scroll_distance:300
+      });            
+      return this;   
+    },
+    goToFrame : function(frameIndex, duration, callback){
+      this.framesView.goToFrame(frameIndex, duration, callback);
+    }           
   });
 
   return TimelineView;
